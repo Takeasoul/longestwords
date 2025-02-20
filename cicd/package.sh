3c391c198a3b2126d7b817379b660e3a4d09e4e3
Takeasoul <117304175+Takeasoul@users.noreply.github.com>
2025-02-20 23:21:54 +0300

    Update package.sh

--- cicd/package.sh
+++ cicd/package.sh
@@ -24,16 +24,6 @@ echo "Копирование бинарника..."
 
 cp usr/bin/longest_words ${PACKAGE_NAME}-${VERSION}/usr/bin/
 
- 
-
-echo "Создание control файла..."
-
-echo "Package: ${PACKAGE_NAME}" > ${PACKAGE_NAME}-${VERSION}/DEBIAN/control
-echo "Version: ${VERSION}" >> ${PACKAGE_NAME}-${VERSION}/DEBIAN/control
-echo "Architecture: ${ARCH}" >> ${PACKAGE_NAME}-${VERSION}/DEBIAN/control
-echo "Maintainer: KIRIKITA <odenter2003@gmail.com>" >> ${PACKAGE_NAME}-${VERSION}/DEBIAN/control
-echo "Description: Программа для нахождения самых длинных слов в каждой фразе текста" >> ${PACKAGE_NAME}-${VERSION}/DEBIAN/control
-
 
  
 
